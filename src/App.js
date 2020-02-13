181f26cfe61dd38c7e33bd12ac0f4096771de002
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-13 09:02:18 +0300

    update the Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home' 
+import Home from './components/Home'
+import PostDetails from './components/PostDetails' 
 
 function App() {
   return (
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path='/post/:id' component={PostDetails} />
         </Switch>
       </div>
   )
