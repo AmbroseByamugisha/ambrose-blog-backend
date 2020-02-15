80d7434a6cafb2f7d492d38ba049805049941e8c
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-15 12:17:16 +0300

    add the create post form

---
+++ src/components/CreatePost.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { addPost } from '../actions'
+import { connect } from 'react-redux'
+
+class CreatePost extends Component {
+    state = { body: "", title: "", editing: false}
+    
+    handleTitleChange = ({ target }) => {
+        this.setState({ title: target.value })
+    }
+
+    handleBodyChange = ({ target }) => {
+        this.setState({ body: target.value })
+    }
+
+    // post title input and truncate the body
+    handleSubmit = (event) => {
+
+        const { dispatch } = this.props
+        //const { body, title } = this.state
+        
+        dispatch(addPost(this.state))
+        console.log(this.state)
+        event.preventDefault()
+        }
+        render() {
+            return(
+                <div>
+                <form>
+                <input onChange={this.handleTitleChange} placeholder="post title"  name="title" />
+                <input onChange={this.handleBodyChange} placeholder="What's on your mind"  name="body" />
+                <button onClick={this.handleSubmit}>Submit</button>
+                </form>
+                </div>
+            )
+        }
+
+}
+
+export default connect()(CreatePost)
