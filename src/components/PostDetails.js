f3ab6805cf72b08a34e6ec7fd0326dafcc05a519
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-15 12:13:36 +0300

    update the edit button

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -2,29 +2,45 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { firestoreConnect } from 'react-redux-firebase'
+import { deletePost, editPost } from '../actions'
+import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const PostDetails = (props) => {
-    const {post} = props
+    const {post, deletePost} = props
     if(post){
     return (
         <div className="post">
-          {post ? post.map((post) => (!post.editing ?
+          {post.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
           <h5>{post.body}</h5>
-          <button>Delete</button>
-        </div> : null
-        )): <h3>Loading...</h3>
+          <Link className='secondary-content' to='/' onClick={ () => deletePost(post.id) }>
+							<i className='material-icons delete'>delete</i>
+						</Link>
+            <Link className='secondary-content' to={{
+              pathname: '/update_post',
+              state: {postId: post.id}
+            }}>
+            <i className='material-icons edit'>edit</i>
+            </Link>        
+          </div> 
+        ))
           }
         </div>
-    )} else {
-      return (
-        <div className="container center">
-          <p>Loading post...</p>
-        </div>
+    )}else {
+      return(
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location,
+                     isdeleted: false }
+          }}
+        />
       )
-    }
-}
+      } 
+  }
+
 
 // CREATE A LOADING STATE FOR DATA FETCHING
 // COZ THE STATE RELOADS AS SERVER REFRESHES
@@ -38,9 +54,18 @@ const mapStateToProps = (state, ownProps) => {
       post: post
     }
   }
-  export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([{
-      collection: 'posts'
-    }])
-  )(PostDetails)
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (post_id) => { dispatch(deletePost(post_id))},
+    editPost: (post_id) => { dispatch(editPost(post_id))}
+  }
+}
+  
+  
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect([{
+    collection: 'posts'
+  }])
+)(PostDetails)
