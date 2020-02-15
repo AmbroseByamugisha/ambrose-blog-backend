2d40c6d26119d40a3e35c8de5ac3936bb4ffc44f
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-15 12:12:54 +0300

    updated the routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import PostDetails from './components/PostDetails' 
+import EditPost from './components/EditPost'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path='/post/:id' component={PostDetails} />
+          <Route path='/update_post' component={EditPost} />
         </Switch>
       </div>
   )
