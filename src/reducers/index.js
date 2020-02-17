4a7581d0cdda566015749a3d0b4310e7bed8de39
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-17 10:31:27 +0300

    add the signup reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 import posts from './posts';
+import auth from './auth'
 
 export default combineReducers({
   posts,
+  auth,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
 });
