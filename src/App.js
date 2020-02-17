95f1e16a9ed197b162ea5e720163b90de8873027
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-17 10:28:17 +0300

    update the signup route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import PostDetails from './components/PostDetails' 
 import EditPost from './components/EditPost'
+import SignUp from './components/SignUp'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path='/post/:id' component={PostDetails} />
           <Route path='/update_post' component={EditPost} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
   )
