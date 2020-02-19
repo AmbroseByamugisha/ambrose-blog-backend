382be7c01ed90f18a71088bf5a04af86a9270964
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-19 11:39:02 +0300

    update the edit post route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import PostDetails from './components/PostDetails' 
 import EditPost from './components/EditPost'
 import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Navbar from './components/Navbar'
+import ProtectedRoute from './components/ProtectedRoute'
 
-function App() {
+function App(props) {
+  const { authUID } = props
   return (
       <div>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path='/post/:id' component={PostDetails} />
-          <Route path='/update_post' component={EditPost} />
+          {/* <Route path='/update_post' component={EditPost} /> */}
           <Route path='/signup' component={SignUp} />
+          <Route path='/login' component={Login} />
+          <ProtectedRoute
+            exact
+            path="/update_post"
+            component={EditPost}
+            authUID={authUID}
+          />
         </Switch>
       </div>
   )
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    authUID: state.firebase.auth.uid
+  }
+}
+
+export default connect(mapStateToProps)(App)
