39706d490bb6625f49e0fe95e5fc8ce3d3d6ffe6
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-20 13:13:52 +0300

    update the logout link

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { signOut } from '../actions'
+import '../index.css'
 
 const SignedInLinks = (props) => {
   return (
     <div>
       <ul className="right">
-        <li><a onClick={props.signOut}>Log Out</a></li>
+        <li onClick={props.signOut} className="pointer">Log Out</li>
       </ul>
     </div>
   )
