af65f8134eea22d2b21bd9fc35a6ad487549c8e6
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-20 13:12:54 +0300

    update the home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,22 @@ import React from 'react'
 import CreatePost from './CreatePost'
 import AllPosts from './AllPosts'
 import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function Home(props){
     const { auth } = props
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.root}>
             {auth.uid ? <CreatePost /> : null}
             <AllPosts />
         </div>
