0732d9ace38cb3bc56abe8b28fe6491b54f3e988
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-21 13:11:09 +0300

    update the links

--- src/components/SignedOutLinks.js
+++ src/components/SignedOutLinks.js
@@ -5,7 +5,6 @@ const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/signup'>Signup</NavLink></li>
         <li><NavLink to='/login'>Login</NavLink></li>
       </ul>
     </div>
