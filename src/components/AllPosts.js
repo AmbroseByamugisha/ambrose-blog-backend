40671a5ffd954467833570e319cd9a731e104f9d
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-21 13:07:46 +0300

    update the create post form

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -7,6 +7,7 @@ import PostSummary from './PostSummary'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import '../index.css'
+import CreatePost from './CreatePost'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -24,7 +25,7 @@ const useStyles = makeStyles(theme => ({
 // KEEP THE LINK TO THE POST, NOT THE WHOLE SPACE AROUND THE POST  
 const AllPosts = (props) => {
     const classes = useStyles();
-    const {posts} = props
+    const {posts, auth} = props
     //posts.sort(function(a,b){return a.post.date.seconds-b.post.date.seconds}) 
     return (
         <div className={classes.root}>
@@ -39,7 +40,7 @@ const AllPosts = (props) => {
           
         </Grid>
       </Grid>
-        
+      {auth.uid ? <CreatePost /> : null}
       { posts ? posts.map(post => {
         return (
           <Link to={'/post/' + post.id} key={post.id}>
@@ -55,7 +56,8 @@ const mapStateToProps = (state) => {
 
     return {
         //postsList: state.posts.posts,
-      posts: state.firestore.ordered.posts
+      posts: state.firestore.ordered.posts,
+      auth: state.firebase.auth
     }
   }
 
